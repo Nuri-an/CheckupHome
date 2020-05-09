451b83dce99ef55007fb3e4d59e35635182f9897
Nurian <nuriancoelho@hotmail.com>
2020-05-09 15:43:15 -0300

    page view

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<header>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title> Check-up Home</title>
+    <link rel="shortcut icon" href="../mobile/assets/icon.png" type="image/x-png" />
+</header>
+
+<body style="background-color: #f5f5f5; overflow-x: hidden;">
+    <div style="width: 100vw; height: 100vh; margin-top: 30px; align-items: center; text-align: center;">
+        <h1 class="text-muted" style="text-align: center;"> Check-up Home</h1>
+        <div style="align-items: center;">
+        <img src="./assets/download.png" style="height: 300px; margin-top: 100px; margin-right: 150px;">
+        <a onclick="window.open('C:/Users/USUARIO/Downloads/bethehero.apk')" href="#instalacao" style="text-align: center; margin-top: 150px;" class="btn btn-outline-warning"> 
+            Download for android
+        </a>
+        </div>
+        <br>
+        <h5 style="margin-top: 50px"> Disponível apenas em plataformas android</h5>
+
+    </div>
+
+    <div id="instalacao" style="width: 100vw; height: auto; padding-bottom: 50px; background-color: #ffeebe;">
+        <h3 class="text-muted container" style="padding-top: 100px; "> Instalação </h3>
+        <h5 class="text-justify container font-weight-normal" style="margin-top: 100px">
+            Após fazer o download, você deve ativar a permissão “Fontes desconhecidas”, para isso:
+            <br> <br> <br>
+            - No Android 7.0 Nougat ou anterior
+            <br> <br>
+            <ul>
+                <li>Acesse o menu de configurações do seu telefone através da barra de notificações ou pelo ícone do app;</li>
+                <li>Agora, toque na opção de Tela de Bloqueio e segurança (em alguns dispositivos essa opção pode se chamar apenas Segurança)</li>
+                <li>No novo menu, procure pela opção Fontes desconhecidas; ao encontrá-la, mude a chave da opção para a posição de ligado;</li>
+                <li>No alerta para Fontes desconhecidas, toque em Ok para confirmar a sua decisão;</li>
+                <li>Pronto, você deu permissão para que aplicativos vindos de fontes não oficiais, fora da Play Store, possam ser instalados.</li>
+            </ul>
+            <br> <br>
+            - No Android 8.0 Oreo ou superior
+            <br> <br>
+            <ul>
+                <li>Acesse o menu de configurações do seu telefone através da barra de notificações ou pelo ícone do app;</li>
+                <li>Agora, toque na opção Aplicativos e notificações;</li>
+                <li>Toque em Acesso especial;</li>
+                <li>Deslize a tela para baixo e selecione a opção Instalar aplicativos desconhecidos;</li>
+                <li>Agora, toque no app que deseja autorizar como fonte da instalação; utilizaremos o Google Chrome como exemplo;</li>
+                <li>Ative a chave da opção Permitir desta fonte;</li>
+                <li>Pronto, você deu permissão para que aplicativos vindos de fontes não oficiais, fora da Play Store, possam ser instalados.</li>
+            </ul>
+            <br> <br>
+            Agora, toque no arquivo baixado e proceda com a instalação normalmente, dando as permissões necessárias de acesso.
+        </h5>
+    </div>
+</body>
+
+</html>
